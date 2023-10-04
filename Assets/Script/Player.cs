a01ff5f754421233b00d3f31d61bd0c881f46f05
AFERREIRA33 <92852107+AFERREIRA33@users.noreply.github.com>
2023-10-04 21:43:48 +0200

    block player on the screen

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Vector2 screenBounds;
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     Vector2 movement;
@@ -11,7 +13,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
     }
 
     // Update is called once per frame
@@ -26,20 +28,30 @@ public class Player : MonoBehaviour
             rb.rotation = Mathf.Clamp(rb.rotation, -60, 60);
             rb.MoveRotation(rb.rotation - rotation * 50 * moveSpeed * Time.fixedDeltaTime);
         }
-        /*else if (rb.rotation > 60f)
-        {
-            rb.SetRotation(60f);
-        } else if (rb.rotation < -60f)   && rb.rotation <= 60f && rb.rotation >= -60f
-        {
-            rb.SetRotation(-60f);
-        }*/
     }
 
     private void FixedUpdate()
     {
-        
+        if (transform.position.x >= screenBounds.x)
+        {
+            transform.position = new Vector2(screenBounds.x, transform.position.y);
+        }
+        if (transform.position.x <= screenBounds.x * -1)
+        {
+            transform.position = new Vector2(screenBounds.x * -1, transform.position.y);
+        }
+        if (transform.position.y >= screenBounds.y)
+        {
+            transform.position = new Vector2(transform.position.x, screenBounds.y);
+        }
+        if (transform.position.y <= screenBounds.y * -1)
+        {
+            transform.position = new Vector2(transform.position.x, screenBounds.y * -1);
+        }
         transform.Translate(movement * moveSpeed * Time.fixedDeltaTime);
-        //Vector2 rbPos = new Vector2(rb.position.x, rb.position.y);
-        //rb(rbPos + movement * moveSpeed * Time.fixedDeltaTime);
+        /*Vector2 objPos = transform.position;
+        objPos.x = Mathf.Clamp(objPos.x + movement.x, screenBounds.x, screenBounds.x * -1) - objPos.x;
+        objPos.y = Mathf.Clamp(objPos.y + movement.y, screenBounds.y, screenBounds.y * -1) - objPos.y;
+        transform.Translate(objPos * moveSpeed * Time.fixedDeltaTime);*/
     }
 }
